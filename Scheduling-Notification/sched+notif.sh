c25e168aab0b41e5f868fc233efb45ad9dfa8a47
root <root@localhost.localdomain>
2020-09-04 04:56:15 -0700

    sch+not

---
+++ Scheduling-Notification/sched+notif.sh
@@ -0,0 +1,2 @@
+34 20 * * * /root/scripting-repo/Scheduling-Notification | mail -s "connectivity status" you_mal@adress.com
+
---
+++ Scheduling-Notification/sched.sh
@@ -0,0 +1,5 @@
+# paste this content is crontab -e
+# 34 20 * * * your_script_path > save_folder
+
+34 20 * * * /root/scripting-repo/Scheduling-Notification > output_folder
+
